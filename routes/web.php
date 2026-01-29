18edaebb0a0b9002ca00b363ca561d3c3ffde5ee
Rainer J Fronz <65880191+rainerfronz@users.noreply.github.com>
2026-01-29 14:22:16 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,19 @@
+
 <?php
 
 use Illuminate\Support\Facades\Route;
 
+/*
+Kid version:
+These are the website pages people can visit.
+*/
+
+// Health check (used to confirm the app is alive)
 Route::get('/health', function () {
     return 'OK';
 });
 
+// Temporary home page test
 Route::get('/', function () {
     return 'LCR is alive';
-});<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
 });
