3e177d30c5ec401b81fe59bb1d22a80b243b6de4
Rainer J Fronz <65880191+rainerfronz@users.noreply.github.com>
2026-01-29 14:13:11 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/health', function () {
+    return 'OK';
+});
+
+Route::get('/', function () {
+    return 'LCR is alive';
+});<?php
+
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
